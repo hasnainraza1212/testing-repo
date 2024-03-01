dfb8e75f71b6961617c9b25b6775195407395e04
hasnainraza1212 <hr961992@gmail.com>
2024-03-01 17:22:13 +0500

    ok

--- test-c/src/pages/Login/Login.jsx
+++ test-c/src/pages/Login/Login.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 const Login = () => {
+    useEffect(()=>{
+        (async()=>{
+            const res= await axios.get("https://prod-test-hr.cyclic.app/api/v1/login")
+            console.log(res.data)
+            })()
+    },[])
   return (
     <div>Login</div>
   )
