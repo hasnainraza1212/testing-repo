755cdf2ac679c637ab80f5bb745f45b0b153b487
hasnainraza1212 <hr961992@gmail.com>
2024-03-01 20:03:36 +0500

    ok

--- test-b/index.js
+++ test-b/index.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import { posts } from "./posts.js"
+import session from 'express-session';
+import MemoryStore from 'memorystore';
 const app = express()
 const PORT = process.env.PORT
 app.use(cors({
@@ -9,18 +11,40 @@ app.use(cors({
   "preflightContinue": false,
   "optionsSuccessStatus": 204
 }))
-const handleSession = (req, res, next)=>{
-    if  (req.session?.id){
-      return  next()
-    }
-    else{
-        res.status(401).send({message:"Unauthorized", success:false})
-    }
-}
-app.use(handleSession)
-app.get("/", (req, res)=>{
-    res.status(200).send({message:"hello from server"})
-})
+// const handleSession = (req, res, next)=>{
+//     if  (req.session?.id){
+//       return  next()
+//     }
+//     else{
+//         res.status(401).send({message:"Unauthorized", success:false})
+//     }
+// }
+// app.use(handleSession)
+const MemoryStoreInstance = MemoryStore(session);
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  store: new MemoryStoreInstance({
+    checkPeriod: 86400000 // prune expired entries every 24h
+  })
+}));
+
+// Define routes
+app.get('/', (req, res) => {
+  if (req.session.views) {
+    req.session.views++;
+    res.send(`You visited this page ${req.session.views} times.`);
+  } else {
+    req.session.views = 1;
+    res.send('Welcome to the session example. Refresh the page!');
+  }
+  console.log(MemoryStoreInstance)
+});
+
+// app.get("/", (req, res)=>{
+//     res.status(200).send({message:"hello from server"})
+// })
 app.get("/api/v1/", (req, res)=>{
     res.status(200).send(posts)
 })
