00435d161225b90442324c2caf95271924e5182c
HasnainRaza <105921794+hasnainraza1212@users.noreply.github.com>
2024-03-01 16:03:44 +0500

    Update vite.config.js

--- test-c/vite.config.js
+++ test-c/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api/v1': 'https://prod-test-hr.cyclic.app',
+      '/api/v1':{
+        target: 'https://prod-test-hr.cyclic.app',
+        changeOrigin: true,
+      },
     }}
 })
